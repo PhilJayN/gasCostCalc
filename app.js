258909db50a2b438f3c72084ed60d16be1503982
phil n <surkins@gmail.com>
2016-10-31 13:52:17 -0700

    Add document ready function. Finally, about time I did it!

--- app.js
+++ app.js
@@ -1,80 +1,88 @@
-
-
-//create click handler for calcuate button element:
-var $calculateButtonEl = $(".calculate");
-console.log ($calculateButtonEl);
-$calculateButtonEl.on("click", function(){
-  console.log ('hi');
-});
-
 //calculate button click counter
 var clickCount = 0;
 function calcButtonClickCounter () {
 clickCount ++;
 }
 
-function calculateButtonPressed () {
-calcButtonClickCounter();
 
-//get IDs of elements:
-var cashPrice = document.getElementById("cashPrice").value;
-var creditPrice = document.getElementById("creditPrice").value;
-var bankDiscount = document.getElementById("bankDiscount").value;
-var gallons = document.getElementById("gallonsNeeded").value;
+var main = function () {
+  "use strict";
+//code below is inside main function:
+//create click handler for calculate button element and run code when it is clicked
+var $calculateButtonEl = $(".calculate");
+console.log ($calculateButtonEl);
+$calculateButtonEl.on("click", function(){
+  console.log ('hi');
 
-//calculations:
-var totalCash = parseInt(gallons) * parseFloat(cashPrice);
-var totalCredit = parseInt(gallons) * parseFloat(creditPrice);
-var totalCreditWDiscount = totalCredit - totalCredit * bankDiscount / 100;
-var difference;
-//substracting larger value from smaller result in negative, therefore use absolute value:
-if (totalCreditWDiscount < cashPrice) {
-  difference = Math.abs(totalCreditWDiscount - totalCash);
+  //run to keep track to print out num of runs to user (Run #1, run #2, etc...)
+  calcButtonClickCounter();
+
+  //get IDs of elements:
+  var cashPrice = document.getElementById("cashPrice").value;
+  var creditPrice = document.getElementById("creditPrice").value;
+  var bankDiscount = document.getElementById("bankDiscount").value;
+  var gallons = document.getElementById("gallonsNeeded").value;
+
+  //calculations:
+  var totalCash = parseInt(gallons) * parseFloat(cashPrice);
+  var totalCredit = parseInt(gallons) * parseFloat(creditPrice);
+  var totalCreditWDiscount = totalCredit - totalCredit * bankDiscount / 100;
+  var difference;
+  //substracting larger value from smaller result in negative, therefore use absolute value:
+  if (totalCreditWDiscount < cashPrice) {
+    difference = Math.abs(totalCreditWDiscount - totalCash);
+  }
+  else {
+    difference = totalCreditWDiscount - totalCash;
+  }
+
+  console.log('totalCreditWDiscount $:', totalCreditWDiscount);
+
+  console.log ('totalCredit', totalCredit);
+
+
+  //use jQuery to create elements and put in calculation results:
+  // var cash = $("<p>").text('Cost of using cash: $' + totalCash);
+  // var credit = $("<p>").text('Cost of using credit with bank discount: $' + totalCreditWDiscount);
+  var $totalCash = $("<td>").text(totalCash);
+  var $totalCredit =  $("<td>").text(totalCredit);
+  var $totalCreditWDiscount = $("<td>").text(totalCreditWDiscount);
+  $("#cashTr").append($totalCash);
+  $("#creditTr").append($totalCredit);
+  $("#creditAndDiscountTr").append($totalCreditWDiscount);
+  $("#diffTr").append(difference);
+
+  //jQuery create element template to put in FINAL conclusion:
+  var elConclusion = $("<p>");
+  var conclusion = 'You can shave approximately ' + (difference*100).toFixed(2) + ' cents off your total bill by using ' ;
+
+  //code below runs depending on whether it's cheaper to pay with cash or credit:
+  if (totalCreditWDiscount < totalCash) {
+    elConclusion.text(conclusion + 'credit card!');
+  }
+  else {
+    elConclusion.text(conclusion + 'cash!');
+  }
+  $(".calculationResultContainer").append(elConclusion);
+
+  //finally, after calculation done, unhide the hiddenResultTable:
+  $(".hiddenResultTable").css("visibility", "visible");
+
+  ///run #
+   var runNumber = $("<p>").text("Run #" + clickCount);
+     $(".calculationResultContainer").append(runNumber);
+
+////closing of calculate button function syntax:
+});
+
+///closing of main function syntax:
 }
-else {
-  difference = totalCreditWDiscount - totalCash;
-}
-
-console.log('totalCreditWDiscount $:', totalCreditWDiscount);
-
-console.log ('totalCredit', totalCredit);
+$(document).ready(main);
+///end of document ready function//
 
 
 
-//use jQuery to create elements and put in calculation results:
-// var cash = $("<p>").text('Cost of using cash: $' + totalCash);
-// var credit = $("<p>").text('Cost of using credit with bank discount: $' + totalCreditWDiscount);
-var $totalCash = $("<td>").text(totalCash);
-var $totalCredit =  $("<td>").text(totalCredit);
-var $totalCreditWDiscount = $("<td>").text(totalCreditWDiscount);
-$("#cashTr").append($totalCash);
-$("#creditTr").append($totalCredit);
-$("#creditAndDiscountTr").append($totalCreditWDiscount);
-$("#diffTr").append(difference);
 
-//jQuery create element template to put in FINAL conclusion:
-var elConclusion = $("<p>");
-var conclusion = 'You can shave approximately ' + (difference*100).toFixed(2) + ' cents off your total bill by using ' ;
-
-//code below runs depending on whether it's cheaper to pay with cash or credit:
-if (totalCreditWDiscount < totalCash) {
-  elConclusion.text(conclusion + 'credit card!');
-}
-else {
-  elConclusion.text(conclusion + 'cash!');
-}
-$(".calculationResultContainer").append(elConclusion);
-
-//finally, after calculation done, unhide the hiddenResultTable:
-$(".hiddenResultTable").css("visibility", "visible");
-
-///run #
- var runNumber = $("<p>").text("Run #" + clickCount);
-   $(".calculationResultContainer").append(runNumber);
-
-
-
-}
 
 
 ////Final verdict: Maybe put this in an organized table, pros and cons, people prob dont like to read long P!.  Typically, for most gas stations in the South Bay area, cash prices are around ten cents less for many reasons. One reason is to encourage people to get inside to buy snacks and spend more. But hot Cheetos aside, you can save some money by paying with cash. However, it depends on how much you value your time. Of course, paying by cash requires you to walk inside, and possibly wait a while to pay, whereas credit card is usualy quicker and more convienient.
