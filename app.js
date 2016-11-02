c7bd28acae7984c39aed898d29d3ba980ddf7b2c
phil n <surkins@gmail.com>
2016-11-01 23:10:43 -0700

    Organize code and about to put a for loop that dynamically create an html table when user clicks on calculate button

--- app.js
+++ app.js
@@ -4,45 +4,16 @@ function calcButtonClickCounter () {
   clickCount ++;
 }
 
-///testing table creation template
-function createResultTable () {
-  //first, create elements:
-  $(".calculationResultContainer").append("<table></table>");
-    var $table = $(".calculationResultContainer").children(); ///this holds a jQuery obj. (<table> element is created, but dangling)
-      var $row1 = $table.append( $("<tr><td>A</td><td>B</td></tr>") );
-      var $row2 = $table.append( $("<tr><td>Testdata</td></tr>") );
-      var $row3 = $table.append( $("<tr></tr>") );
-      // var $row4 = $table.append( $("<tr><p>jasdklf</p></tr>") );
-    var testData = 3;
-
-//$('element').attr('id', 'value');
-    // $("row3").attr("id", 'testId');
-
-$("#testId").text('hi!');
-    // $row4.append("<td></td>").text(testData);
-    // $row1.text('hi');
-  //   var $tHeader = $("<th>").text("table header");
-  //   var $tableData = $("<td>").text("test");
-  //
-  // //second, target those newly created elements, and append other elements to it:
-  //   $("table").append($row);
-  //   $("tr").append($tableData);
-  //   $("tr").append($tHeader);
-
-  //finally, target calculationResultContainer div and append everything to it:
-    // $(".calculationResultContainer").append($table);
-
-}
-
-
 var main = function () {
   "use strict";
-//code below is inside main function:
+  console.log("Document ready, js loaded.");
+
+//all code below is inside main function:
+  var priceArray = [], tableDataArray = [];
 
 //create click handler for calculate button element and run code when it is clicked
   var $calculateButtonEl = $(".calculate");
   $calculateButtonEl.on("click", function(){
-  console.log ('Test');
 
   //run to keep track to print out num of runs to user (Run #1, run #2, etc...)
   calcButtonClickCounter();
@@ -65,6 +36,19 @@ var main = function () {
   else {
     difference = totalCreditWDiscount - totalCash;
   }
+
+//push calculation result and table data title into arrays:
+  priceArray.push(totalCash, totalCredit, totalCreditWDiscount, difference);
+  console.log('price arr', priceArray);
+  tableDataArray.push("Cash Price", "Credit Price", "Credit Price with Discount", "Difference");
+  console.log(tableDataArray);
+
+
+
+
+
+
+
 
   //use jQuery to create elements and put in calculation results:
   var $totalCash = $("<td>").text(totalCash);
@@ -104,8 +88,30 @@ $(document).ready(main);
 ///end of document ready function//
 
 
-///testing table creation template
-createResultTable();
+//
+// ///testing table creation template
+// function createResultTable () {
+//   //first, create elements:
+//   $(".calculationResultContainer").append("<table></table>");
+//     // var $table = $(".calculationResultContainer").children(); ///this holds a jQuery obj. (<table> element is created, but dangling)
+//     //   var $row1 = $table.append( $("<tr><td>A</td><td>B</td></tr>") );
+//     //   var $row2 = $table.append( $("<tr><td>Testdata</td></tr>") );
+//     //   var $row3 = $table.append( $("<tr></tr>") );
+//       // var $row4 = $table.append( $("<tr><p>jasdklf</p></tr>") );
+//     // $row4.append("<td></td>").text(testData);
+//     // $row1.text('hi');
+//   //   var $tHeader = $("<th>").text("table header");
+//   //   var $tableData = $("<td>").text("test");
+//   //
+//   // //second, target those newly created elements, and append other elements to it:
+//   //   $("table").append($row);
+//   //   $("tr").append($tableData);
+//   //   $("tr").append($tHeader);
+//
+//   //finally, target calculationResultContainer div and append everything to it:
+//     // $(".calculationResultContainer").append($table);
+//
+// }
 
 ////Final verdict: Maybe put this in an organized table, pros and cons,
 //people prob dont like to read long P!.  Typically, for most gas stations
@@ -122,132 +128,50 @@ createResultTable();
 //add more features at end of app:
 //kickback points consideration, or other rewards.(maybe do a check box that user can click to incl. calc)
 
-//create new dangling table and tr element
-var $newTable = $("<table>");
-var tr1 = $("<tr>");
-var tr2 = $("<tr>");
-var tr3 = $("<tr>");
-var tr4 = $("<tr>");
-
-//append table rows to table element.
-$newTable.append(tr1);
-$newTable.append(tr2);
-$newTable.append(tr3);
-$newTable.append(tr4);
-///so far, $newTable looks like: <table> <tr></tr> </table>
-
-//create new dangling table data element
-var td1 = $("<td>");
-var td2 = $("<td>");
-var td3 = $("<td>");
-var td4 = $("<td>");
-
-//append table data element to rows
-tr1.append(td1);
-tr1.append(td2);
-tr1.append(td3);
-tr1.append(td4);
-
-
-td1.text('td1 text!');
-td2.text('td2 text!');
-td1.text('td3 text!');
-td2.text('td4 text!');
-
-$("#resultTable").append($newUl);
-
-
+// //create new dangling table and tr element
+// var $newTable = $("<table>");
+// var tr1 = $("<tr>");
+// var tr2 = $("<tr>");
+// var tr3 = $("<tr>");
+// var tr4 = $("<tr>");
+//
+// //append table rows to table element.
+// $newTable.append(tr1);
+// $newTable.append(tr2);
+// $newTable.append(tr3);
+// $newTable.append(tr4);
+// ///so far, $newTable looks like: <table> <tr></tr> </table>
+//
+// //create new dangling table data element
+// var td1 = $("<td>");
+// var td2 = $("<td>");
+// var td3 = $("<td>");
+// var td4 = $("<td>");
+//
+// //append table data element to rows
+// tr1.append(td1);
+// tr1.append(td2);
+// tr1.append(td3);
+// tr1.append(td4);
+//
+//
+// td1.text('td1 text!');
+// td2.text('td2 text!');
+// td1.text('td3 text!');
+// td2.text('td4 text!');
+//
+// $("#resultTable").append($newUl);
 
 
-
-
-
-
-
-
-
-
-
-
-
-//create new dangling table and tr element
-var $newTable = $("<table>");
-var tr1 = $("<tr>");
-var tr2 = $("<tr>");
-var tr3 = $("<tr>");
-var tr4 = $("<tr>");
-
-//append table rows to table element.
-$newTable.append(tr1);
-$newTable.append(tr2);
-$newTable.append(tr3);
-$newTable.append(tr4);
-///so far, $newTable looks like: <table> <tr></tr> </table>
-
-//create new dangling table data element
-var td1 = $("<td>");
-var td2 = $("<td>");
-var td3 = $("<td>");
-var td4 = $("<td>");
-var test = $("<td>");
-
-
-//append table data element to rows
-tr1.append(td1);
-tr1.append(test);
-
-tr2.append(td2);
-tr3.append(td3);
-tr4.append(td4);
-
-
-td1.text('td1 text!');
-td2.text('td2 text!');
-td3.text('td3 text!');
-td4.text('td4 text!');
-
-$("#resultTable").append($newTable);
-
-console.log('test');
-
-
-//create new dangling table and tr element
-var $newTable = $("<table>");
-var tr1 = $("<tr>");
-var tr2 = $("<tr>");
-var tr3 = $("<tr>");
-var tr4 = $("<tr>");
-
-//append table rows to table element.
-$newTable.append(tr1);
-$newTable.append(tr2);
-$newTable.append(tr3);
-$newTable.append(tr4);
-///so far, $newTable looks like: <table> <tr></tr> </table>
-
-//create new dangling table data element
-var td1 = $("<td>");
-var td2 = $("<td>");
-var td3 = $("<td>");
-var td4 = $("<td>");
-var test = $("<td>");
-
-
-//append table data element to rows
-tr1.append(td1);
-tr1.append('a');
-tr1.append('b');
-
-tr2.append(td2);
-tr3.append(td3);
-tr4.append(td4);
-
-
-td1.text('td1 text!');
-td2.text('td2 text!');
-td3.text('td3 text!');
-td4.text('td4 text!');
-
-$("#resultTable").append($newTable);
-
-console.log('test');
+// var table = '';
+// var rows = 4;
+// var cols = 3;
+//
+// for (var r=0; r < rows; r++) {
+//   table += "<tr>";
+//     for (var c=0; c < cols; c++ ) {
+//       table += "<td>" + c + "</td>";
+//     }
+//   table += "</tr>";
+// }
+// document.write(table);
